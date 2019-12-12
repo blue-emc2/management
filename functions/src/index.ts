e312d250ca07bf98fd93a10b3ee82b6e3af8db8e
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-12-12 21:46:24 +0900

    問題を保存するAPIを実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,21 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+admin.initializeApp();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
+});
+
+export const createQuestion = functions.https.onRequest(async (req, res) => {
+  const { question } = req.body;
+  const db = admin.firestore();
+  const ref = db.collection('/questions');
+  await ref
+    .add({
+      question,
+    })
+    .then(() => {
+      res.status(200).send('OK!'); // TODO: 後でよしなにする
+    });
 });
