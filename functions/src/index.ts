62ff1a4ae7862628b1ce0bfb56f682b24ea9f599
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-12-13 19:08:34 +0900

    stateを更新するAPIを実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,17 @@
 import * as functions from 'firebase-functions';
 import admin from 'firebase-admin';
 
+// enum State {
+//   Standby = 0,
+//   JoinWaiting,
+//   JoinFinished,
+//   QuestionDeliver,
+//   AnswerWaiting,
+//   AnswerFinished,
+// }
+
 admin.initializeApp();
+const db = admin.firestore();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
@@ -9,7 +19,6 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 
 export const createQuestion = functions.https.onRequest(async (req, res) => {
   const { question } = req.body;
-  const db = admin.firestore();
   const ref = db.collection('/questions');
   await ref
     .add({
@@ -18,4 +27,13 @@ export const createQuestion = functions.https.onRequest(async (req, res) => {
     .then(() => {
       res.status(200).send('OK!'); // TODO: 後でよしなにする
     });
+});
+
+export const updateState = functions.https.onRequest(async (req, res) => {
+  const ref = db.collection('/management').doc('one');
+  const { state } = req.body;
+
+  ref.set({ state }, { merge: true }).then(() => {
+    res.status(200).send(state);
+  });
 });
