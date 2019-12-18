14804763dbbb108d360e0611a4f3d8ba37372222
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-12-18 10:31:46 +0900

    一つのdocumentに保存される様にした

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,35 +10,52 @@ import admin from 'firebase-admin';
 //   AnswerFinished,
 // }
 
+// 最新のdocを1件返す
+const getManagementLastDocQuery = () =>
+  admin
+    .firestore()
+    .collection('/management')
+    .orderBy('createdAt', 'desc')
+    .limit(1);
+
 admin.initializeApp();
-const db = admin.firestore();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
 
+// 保存先の初期化
 export const initialize = functions.https.onRequest(async (req, res) => {
-  const ref = db.collection('/management').doc();
+  const ref = admin
+    .firestore()
+    .collection('/management')
+    .doc();
 
-  await ref.set({ state: 0 }).then(() => {
-    res.status(200).send(ref.toString());
-  });
-});
-
-export const createQuestion = functions.https.onRequest(async (req, res) => {
-  const { question } = req.body;
-  const ref = db.collection('/questions');
   await ref
-    .add({
-      question,
-    })
+    .set({ state: 0, createdAt: admin.firestore.Timestamp.now() })
     .then(() => {
-      res.status(200).send('OK!'); // TODO: 後でよしなにする
+      res.status(200).send(ref.toString());
     });
 });
 
+// 問題を作る
+export const createQuestion = functions.https.onRequest(async (req, res) => {
+  const { question } = req.body;
+  const ref = getManagementLastDocQuery();
+  const docs = await ref.get().then(snapshot => {
+    return snapshot.docs;
+  });
+
+  docs[0].ref.set({ question }, { merge: true }).then(() => {
+    res.status(200).send('OK!'); // TODO: 後でよしなにする
+  });
+});
+
 export const updateState = functions.https.onRequest(async (req, res) => {
-  const ref = db.collection('/management').doc('one');
+  const ref = admin
+    .firestore()
+    .collection('/management')
+    .doc('one');
   const { state } = req.body;
 
   ref.set({ state }, { merge: true }).then(() => {
