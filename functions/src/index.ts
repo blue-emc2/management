eb0a368ce5bd173e7d7991ca1b7c5ce68ccf067f
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-12-19 20:16:23 +0900

    ユーザー登録と取得のAPIを実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from 'firebase-functions';
 import admin from 'firebase-admin';
 
+// import { User } from './models/user';
+
 // enum State {
 //   Standby = 0,
 //   JoinWaiting,
@@ -32,9 +34,9 @@ export const initialize = functions.https.onRequest(async (req, res) => {
     .doc();
 
   await ref
-    .set({ state: 0, createdAt: admin.firestore.Timestamp.now() })
+    .set({ state: 0, createdAt: admin.firestore.Timestamp.now(), users: [] })
     .then(() => {
-      res.status(200).send(ref.toString());
+      res.status(200).send('OK'); // TODO: 後でよしなにする
     });
 });
 
@@ -51,14 +53,55 @@ export const createQuestion = functions.https.onRequest(async (req, res) => {
   });
 });
 
+// 状態更新
 export const updateState = functions.https.onRequest(async (req, res) => {
-  const ref = admin
-    .firestore()
-    .collection('/management')
-    .doc('one');
   const { state } = req.body;
-
-  ref.set({ state }, { merge: true }).then(() => {
-    res.status(200).send(state);
+  const ref = getManagementLastDocQuery();
+  const docs = await ref.get().then(snapshot => {
+    return snapshot.docs;
   });
+
+  docs[0].ref.set({ state }, { merge: true }).then(() => {
+    res.status(200).send(`OK:${state}`); // TODO: 後でよしなにする
+  });
+});
+
+// ユーザー登録
+export const entry = functions.https.onRequest(async (req, res) => {
+  const { name } = req.body;
+  const ref = getManagementLastDocQuery();
+  const docs = await ref.get().then(snapshot => {
+    return snapshot.docs;
+  });
+
+  // TODO: 人数制限
+  // TODO: name重複チェック
+
+  docs[0].ref
+    .update({
+      users: admin.firestore.FieldValue.arrayUnion({ name }),
+    })
+    .then(() => {
+      res.status(200).send(`OK:${name}`); // TODO: 後でよしなにする
+    });
+});
+
+// ユーザー取得
+export const users = functions.https.onRequest(async (req, res) => {
+  const ref = getManagementLastDocQuery();
+  const snapshot = await ref.get();
+
+  const data = snapshot.docs.map(doc => {
+    if (doc.exists) {
+      return doc.data().users;
+    }
+
+    return null;
+  });
+
+  if (data) {
+    res.status(200).send({ data });
+  } else {
+    res.status(404);
+  }
 });
