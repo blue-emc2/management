3eb4348519b2213c687df32a0d37026733bfc17e
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-12-24 10:24:35 +0900

    firebaseの初期設定を追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import firebase from 'firebase/app';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import firebaseConfig from './firebase-config';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
