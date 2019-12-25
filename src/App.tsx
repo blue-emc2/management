947b8847d3de7f3c342c00a5de5b9242277ffe63
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-12-25 19:08:55 +0900

    ベースを作った

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Card, Button, Segment, Container, Form } from 'semantic-ui-react';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Segment>
+          <Form>
+            <Form.Group>
+              <Form.Button basic size="big" floated="left">
+                はじめる
+              </Form.Button>
+            </Form.Group>
+            <Form.Group inline>
+              <Form.TextArea
+                width={12}
+                placeholder="パンはパンでも食べられないパンはなーんだ"
+              />
+              <Form.Button basic size="big" width={2}>
+                送信
+              </Form.Button>
+            </Form.Group>
+          </Form>
+        </Segment>
+        <Segment>
+          <Card>
+            <Card.Content>
+              <Card.Header>参加者名</Card.Header>
+              <Card.Description>
+                <strong>ここに回答を表示</strong>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <Button basic>回答を開ける</Button>
+            </Card.Content>
+          </Card>
+        </Segment>
+      </Container>
     </div>
   );
 };
